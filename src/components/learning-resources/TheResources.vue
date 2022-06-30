eef3475f2404467d07300c1e6db6741871c6167f
Macbook <macbook@MBP-Macbook.local.tld>
2022-06-30 20:20:25 +0200

    add dynamic components

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,30 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')">Stored resources</base-button>
+        <base-button @click="setSelectedTab('add-resource')">Add resource</base-button>
+    </base-card>
+    <component :is="selectedTab">
+    </component>
+</template>
+
+<script>
+
+import StoredResources from './StoredResources.vue'
+import AddResorce from './AddResorce.vue';
+export default {
+    components: {
+        StoredResources,
+        AddResorce 
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resoorces'
+        }
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
